3c02b769d69647336b02c8304b2778f7bb4655d1
shuvo794 <shuvohossain794@gmail.com>
2022-05-16 11:35:26 -0700

    install a intial pakege

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <h2>Hi buddy</h2> */}
-      <h3>Added</h3>
-      <h3>Added</h3>
+   
+      
     </div>
   );
 }
