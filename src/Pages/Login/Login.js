d78f029b1e98b7cea096a94fd1d58a72e3cbcb5f
shuvo794 <shuvohossain794@gmail.com>
2022-05-21 20:05:14 +0600

    change

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -12,66 +12,100 @@ const Login = () => {
         e.preventDefault()
       }
     return (
-        <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-      <img src={bg} alt='' />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <img src={bg} alt="" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography
+            sx={{ marginTop: "10%", fontWeight: "bold", textAlign: "center" }}
+            variant="h3"
+            gutterBottom
+          >
+            Welcome Back
+          </Typography>
+          <Typography
+            sx={{ marginTop: "5px", textAlign: "center" }}
+            variant="subtitle1"
+            gutterBottom
+          >
+            Start creating and selling NFTs with Mintable
+          </Typography>
+          <form onSubmit={handelLogin}>
+            <Box sx={{ textAlign: "center", width: "100%", marginTop: "30px" }}>
+              <TextField
+                id="standard-basic"
+                label="E-mail/Username"
+                sx={{ width: "60%", marginBottom: "8px" }}
+                variant="standard"
+              />
+
+              <TextField
+                id="standard-basic"
+                label="Password"
+                sx={{ width: "60%", marginBottom: "8px" }}
+                variant="standard"
+              />
+            </Box>
+
+            <Box sx={{ width: "60%", margin: "0px auto" }}>
+              <Button
+                sx={{
+                  backgroundColor: "#8858F4",
+                  color: "#fff",
+                  width: "100%",
+                }}
+                color="inherit"
+                variant="contained"
+              >
+                Log in
+              </Button>
+
+              <Typography
+                sx={{ textAlign: "center", margin: "10px 0px" }}
+                variant="subtitle1"
+              >
+                Or
+              </Typography>
+            </Box>
+            <Box sx={{ width: "80%", margin: "0px auto" }}>
+              <Button
+                sx={{
+                  backgroundColor: "#DB4437",
+                  color: "#fff",
+                  padding: "6px",
+                }}
+                color="inherit"
+                variant="contained"
+              >
+                <FaGoogle className="footer-icons" /> Google
+              </Button>
+
+              <Button
+                sx={{
+                  backgroundColor: "#4267B2",
+                  color: "#fff",
+                  marginLeft: "10px",
+                  padding: "6px",
+                }}
+                color="inherit"
+                variant="contained"
+              >
+                <FaFacebook className="footer-icons" /> Facebook
+              </Button>
+              <Typography
+                sx={{ color: "gray", marginTop: "10px" }}
+                variant="body1"
+              >
+                Already have an account?
+              </Typography>
+              <Button href="/login" sx={{ width: "60%", margin: "0px auto" }}>
+                Login >
+              </Button>
+            </Box>
+          </form>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-      <Typography sx={{marginTop:'10%',fontWeight:'bold',textAlign:'center'}} variant="h3" gutterBottom>
-      Welcome Back
-      </Typography>
-      <Typography sx={{marginTop:'5px',textAlign:'center'}} variant="subtitle1" gutterBottom>
-      Start creating and selling NFTs with Mintable
-      </Typography>
-      <form onSubmit={handelLogin}>
-        <Box sx={{textAlign:'center', width:'100%',marginTop:'30px'}}>
-      <TextField
-       id="standard-basic" 
-       label="E-mail/Username"
-      sx={{width:'60%',marginBottom:'8px'}}
-       variant="standard" />
-        
-     
-        <TextField
-       id="standard-basic" 
-       label="Password"
-       sx={{width:'60%',marginBottom:'8px'}}
-        variant="standard" />
-        </Box>
-       
-
- 
-
-  <Box sx={{ width: '60%',margin:'0px auto'}}>
-  <Button sx={{backgroundColor:'#8858F4',color:'#fff',width: '100%'}}  color='inherit' variant='contained'>
-   Log in
-  </Button>
-
-<Typography sx={{textAlign:'center',margin:'10px 0px'}} variant='subtitle1' >
-Or
-</Typography>
-
-  </Box>
-  <Box sx={{ width: '60%',margin:'0px auto'}}>
-  <Button sx={{backgroundColor:'#DB4437',color:'#fff',padding:'10px'}}  color='inherit' variant='contained'>
-  <FaGoogle className="footer-icons" /> Google 
-  </Button>
-
-  <Button sx={{backgroundColor:'#4267B2',color:'#fff',marginLeft:'10px',padding:'10px'}}  color='inherit' variant='contained'>
-  <FaFacebook className="footer-icons" /> Facebook 
-  </Button>
-  <Typography sx={{color:'gray',marginTop:'10px',}} variant='body1'>
-  Already have an account?
-  </Typography>
- <Button href='/login' sx={{ width: '60%',margin:'0px auto'}}>Login ></Button>
-
-  </Box>
-
-      </form>
-      </Grid>
-      
-      
-    </Grid>
     );
 };
 
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -31,105 +31,146 @@ const SignUp = () => {
   
     return (
       <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-      <img src={bg} alt='' />
+        <Grid item xs={12} md={6}>
+          <img src={bg} alt="" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography
+            sx={{ marginTop: "3px", fontWeight: "bold", textAlign: "center" }}
+            variant="h3"
+            gutterBottom
+          >
+            Create Account
+          </Typography>
+          <Typography
+            sx={{ marginTop: "0px", textAlign: "center" }}
+            variant="subtitle1"
+            gutterBottom
+          >
+            Start creating and selling NFTs with Mintable
+          </Typography>
+          <form onSubmit={handelSignUp}>
+            <Box sx={{ textAlign: "center", width: "100%", marginTop: "30px" }}>
+              <TextField
+                id="standard-basic"
+                label="Username"
+                sx={{ width: "60%", marginBottom: "5px" }}
+                variant="standard"
+              />
+              <br />
+              <TextField
+                id="standard-basic"
+                label="Email"
+                sx={{ width: "60%", marginBottom: "5px" }}
+                variant="standard"
+              />
+              <TextField
+                id="standard-basic"
+                label="Confirm Email"
+                sx={{ width: "60%", marginBottom: "5px" }}
+                variant="standard"
+              />
+              <br />
+              <TextField
+                id="standard-basic"
+                label="Password"
+                sx={{ width: "60%", marginBottom: "5px" }}
+                variant="standard"
+              />
+              <br />
+              <TextField
+                id="standard-basic"
+                label="Confirm Password"
+                sx={{ width: "60%", marginBottom: "5px" }}
+                variant="standard"
+              />
+            </Box>
+            <Box sx={{ width: "60%", margin: "6px auto" }}>
+              <Stepper activeStep={1} alternativeLabel>
+                {steps.map((label) => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </Box>
+
+            <Box sx={{ width: "60%", margin: "6px auto" }}>
+              <Box sx={{ display: "flex", marginBottom: "6px" }}>
+                <Checkbox {...label} Checked />
+                <Typography variant="caption">
+                  By registering you agree with our{" "}
+                  <a href="#">terms and conditions</a> and{" "}
+                  <a href="#">privacy policy</a>.
+                </Typography>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <Checkbox {...label} Checked />
+                <Typography variant="caption">
+                  Don't send me any emails, including when my NFTs sell or when
+                  I buy some
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box sx={{ width: "60%", margin: "0px auto" }}>
+              <Button
+                sx={{
+                  backgroundColor: "#8858F4",
+                  color: "#fff",
+                  width: "100%",
+                }}
+                color="inherit"
+                variant="contained"
+              >
+                Create Account
+              </Button>
+
+              <Typography
+                sx={{ textAlign: "center", margin: "0px 0px" }}
+                variant="subtitle1"
+              >
+                Or
+              </Typography>
+            </Box>
+            <Box sx={{ width: "80%", margin: "0px auto" }}>
+              <Button
+                sx={{
+                  backgroundColor: "#DB4437",
+                  color: "#fff",
+                  padding: "6px",
+                }}
+                color="inherit"
+                variant="contained"
+              >
+                <FaGoogle className="footer-icons" /> Google
+              </Button>
+
+              <Button
+                sx={{
+                  backgroundColor: "#4267B2",
+                  color: "#fff",
+                  marginLeft: "10px",
+                  padding: "6px",
+                }}
+                color="inherit"
+                variant="contained"
+              >
+                <FaFacebook className="footer-icons" /> Facebook
+              </Button>
+              <Typography
+                sx={{ color: "gray", marginTop: "10px" }}
+                variant="body1"
+              >
+                Already have an account?
+              </Typography>
+              <Button href="/login" sx={{ width: "60%", margin: "0px auto" }}>
+                Login >
+              </Button>
+            </Box>
+          </form>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-      <Typography sx={{marginTop:'5px',fontWeight:'bold',textAlign:'center'}} variant="h3" gutterBottom>
-      Create Account
-      </Typography>
-      <Typography sx={{marginTop:'0px',textAlign:'center'}} variant="subtitle1" gutterBottom>
-      Start creating and selling NFTs with Mintable
-      </Typography>
-      <form onSubmit={handelSignUp}>
-        <Box sx={{textAlign:'center', width:'100%',marginTop:'30px'}}>
-      <TextField
-       id="standard-basic" 
-       label="Username"
-      sx={{width:'60%',marginBottom:'5px'}}
-       variant="standard" />
-        <br/>
-        <TextField
-       id="standard-basic" 
-       label="Email"
-       sx={{width:'60%',marginBottom:'5px'}}
-        variant="standard" />
-        <TextField
-       id="standard-basic" 
-       label="Confirm Email"
-      sx={{width:'60%',marginBottom:'5px'}}
-       variant="standard" />
-        <br/>
-        <TextField
-       id="standard-basic" 
-       label="Password"
-       sx={{width:'60%',marginBottom:'5px'}}
-        variant="standard" />
-        <br/>
-        <TextField
-       id="standard-basic" 
-       label="Confirm Password"
-       sx={{width:'60%',marginBottom:'5px'}}
-        variant="standard" />
-        </Box>
-        <Box sx={{ width: '60%',margin:'6px auto', }}>
-      <Stepper activeStep={1} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
-
-  <Box sx={{ width: '60%',margin:'6px auto'}}>
-  <Box sx={{display: 'flex',marginBottom:'6px'}}>
-  <Checkbox {...label} Checked />
-  <Typography variant='caption'>
-  By registering you agree with our <a href='#'>terms and conditions</a> and <a href='#'>privacy policy</a>.
-  </Typography>
-</Box>
-<Box sx={{display: 'flex'}}>
-  <Checkbox {...label} Checked />
-  <Typography variant='caption'>
-  Don't send me any emails, including when my NFTs sell or when I buy some
-  </Typography>
-
- 
-</Box>
-  </Box>
-
-  <Box sx={{ width: '60%',margin:'0px auto'}}>
-  <Button sx={{backgroundColor:'#8858F4',color:'#fff',width: '100%'}}  color='inherit' variant='contained'>
-    Create Account
-  </Button>
-
-<Typography sx={{textAlign:'center',margin:'0px 0px'}} variant='subtitle1' >
-Or
-</Typography>
-
-  </Box>
-  <Box sx={{ width: '60%',margin:'0px auto'}}>
-  <Button sx={{backgroundColor:'#DB4437',color:'#fff',padding:'10px'}}  color='inherit' variant='contained'>
-  <FaGoogle className="footer-icons" /> Google 
-  </Button>
-
-  <Button sx={{backgroundColor:'#4267B2',color:'#fff',marginLeft:'10px',padding:'10px'}}  color='inherit' variant='contained'>
-  <FaFacebook className="footer-icons" /> Facebook 
-  </Button>
-  <Typography sx={{color:'gray',marginTop:'10px',}} variant='body1'>
-  Already have an account?
-  </Typography>
- <Button href='/login' sx={{ width: '60%',margin:'0px auto'}}>Login ></Button>
-
-  </Box>
-
-      </form>
-      </Grid>
-      
-      
-    </Grid>
-     
     );
 };
 
